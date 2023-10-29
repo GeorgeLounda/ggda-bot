824b055e151d5251f0f2ea687e08d25733be77bd
GeorgeLounda <jeffiry94@gmail.com>
2023-10-29 20:29:56 +0800

    正式版ver 1.0

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 
 import { Client, Events, GatewayIntentBits } from "discord.js"
 import dotenv from "dotenv"
-
+import vueInit from "@/core/vue"
+import { loadCommands,loadEvents} from "@/core/loader"
+import { useAppStore } from "./store/app"
+vueInit()
 dotenv.config()
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+loadCommands()
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const appStore = useAppStore()
+appStore.client = client
+loadEvents()
+
+
+
+
 
 // Log in to Discord with your client's token
 client.login(process.env.TOKEN);
