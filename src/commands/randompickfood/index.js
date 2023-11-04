1bb65178500a772e821ba0771c79b75c2a94f752
GeorgeLounda <jeffiry94@gmail.com>
2023-11-04 10:07:35 +0800

    正式版3.2

---
+++ src/commands/randompickfood/index.js
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder } from "discord.js"
+const xlsx = require('xlsx');
+
+export const command = new SlashCommandBuilder()
+.setName('吃什麼')
+.setDescription('隨機抽食物(只有要抽的話不用加東西)')
+
+        
+
+
+export const action = async(interaction) =>{
+   const foodmenu = xlsx.readFile(process.env.EATMENU_PLACE)
+   const sheetNames = foodmenu.SheetNames;
+   const food = foodmenu.Sheets[sheetNames[0]];
+   const range = xlsx.utils.decode_range(food['!ref'])
+   const food_address = {c: 0, r: Math.floor(Math.random() * (range.e.r + 1))};
+   const choose = xlsx.utils.encode_cell(food_address)
+   interaction.reply(food[choose].v)
+   
+
+}
--- src/commands/whattoeat/index.js
+++ src/commands/whattoeat/index.js
@@ -2,26 +2,44 @@ import { SlashCommandBuilder } from "discord.js"
 const xlsx = require('xlsx');
 
 export const command = new SlashCommandBuilder()
-.setName('吃什麼')
-.setDescription('隨機抽食物')
-.addStringOption(option =>
-   option.setName('添加食物')
-      .setDescription('加入食物到抽選名單(如果只是要抽的話不用選填)'))
+.setName('食物名單')
+.setDescription('隨機抽食物(只有要抽的話不用加東西)')
+.addSubcommand(subcommand =>
+   subcommand
+      .setName('添加食物')
+      .setDescription('加入食物到抽選名單')
+      .addStringOption((option)=>
+         option.setName("加入")
+         .setDescription('加入食物到抽選名單')
+         .setRequired(true)
+      ))
+.addSubcommand(subcommand =>
+   subcommand
+      .setName('刪除食物')
+      .setDescription('將食物從抽選名單刪除')
+      .addStringOption((option)=>
+         option.setName("刪除")
+         .setDescription('將食物從抽選名單刪除')
+         .setRequired(true)
+      ))
 
         
 
 
 export const action = async(interaction) =>{
-   const addfood = interaction.options.getString('添加食物') ?? '沒有添加任何東西'
    const foodmenu = xlsx.readFile(process.env.EATMENU_PLACE)
    const sheetNames = foodmenu.SheetNames;
    const food = foodmenu.Sheets[sheetNames[0]];
    const range = xlsx.utils.decode_range(food['!ref'])
-   if (addfood !== '沒有添加任何東西'){
+
+   if (interaction.options.getSubcommand('食物名單') === '添加食物'){
+      const addfood = interaction.options.getString('加入')
       xlsx.utils.sheet_add_aoa(food, [[addfood,interaction.user.tag]], {origin: -1})
       xlsx.writeFile(foodmenu,process.env.EATMENU_PLACE)
-      interaction.reply(addfood + interaction.user.tag)
-   }else{
+      interaction.reply(`${interaction.user.tag} 添加 ${addfood} 成功!! `)
+   }else if (interaction.options.getSubcommand('食物名單') === '刪除食物'){
+      interaction.reply("沒做")
+   }else if (interaction.options.getSubcommand('食物名單') === '查詢食物'){
    const food_address = {c: 0, r: Math.floor(Math.random() * (range.e.r + 1))};
    const choose = xlsx.utils.encode_cell(food_address)
    interaction.reply(food[choose].v)
