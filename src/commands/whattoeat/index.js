10bac1f523f5222fbb82e6db2e34486dca0b698b
GeorgeLounda <jeffiry94@gmail.com>
2023-11-06 02:23:22 +0800

    正式版3.3

--- src/commands/whattoeat/index.js
+++ src/commands/whattoeat/index.js
@@ -22,6 +22,15 @@ export const command = new SlashCommandBuilder()
          .setDescription('將食物從抽選名單刪除')
          .setRequired(true)
       ))
+.addSubcommand(subcommand =>
+   subcommand
+      .setName('查詢食物')
+      .setDescription('查詢名單中的食物跟添加者')
+      .addStringOption((option)=>
+         option.setName("查詢")
+         .setDescription('查詢名單中的食物跟添加者')
+         .setRequired(true)
+      ))
 
         
 
@@ -30,7 +39,7 @@ export const action = async(interaction) =>{
    const foodmenu = xlsx.readFile(process.env.EATMENU_PLACE)
    const sheetNames = foodmenu.SheetNames;
    const food = foodmenu.Sheets[sheetNames[0]];
-   const range = xlsx.utils.decode_range(food['!ref'])
+   var range = xlsx.utils.decode_range(food['!ref'])
 
    if (interaction.options.getSubcommand('食物名單') === '添加食物'){
       const addfood = interaction.options.getString('加入')
@@ -38,11 +47,33 @@ export const action = async(interaction) =>{
       xlsx.writeFile(foodmenu,process.env.EATMENU_PLACE)
       interaction.reply(`${interaction.user.tag} 添加 ${addfood} 成功!! `)
    }else if (interaction.options.getSubcommand('食物名單') === '刪除食物'){
-      interaction.reply("沒做")
+      const deletefood = interaction.options.getString('刪除')
+      for(var food_index = range.s.r;food_index <= range.e.r;food_index++){
+         if(food[xlsx.utils.encode_cell({c: 0, r:food_index})].v === deletefood){
+            for (var replace_index = food_index;replace_index <=range.e.r;replace_index++){
+               food[xlsx.utils.encode_cell({c: 0, r:replace_index})] = food[xlsx.utils.encode_cell({c: 0, r:replace_index+1})]
+               food[xlsx.utils.encode_cell({c: 1, r:replace_index})] = food[xlsx.utils.encode_cell({c: 1, r:replace_index+1})]
+            }
+            range.e.r -= 1
+            food["!ref"] = xlsx.utils.encode_range(range)
+            xlsx.writeFile(foodmenu,process.env.EATMENU_PLACE)
+            interaction.reply(`${interaction.user.tag} 刪除 ${deletefood} 成功!! `)
+            return;
+         }
+       
+      }
+      interaction.reply(`名單中找不到 ${deletefood} `)
    }else if (interaction.options.getSubcommand('食物名單') === '查詢食物'){
-   const food_address = {c: 0, r: Math.floor(Math.random() * (range.e.r + 1))};
-   const choose = xlsx.utils.encode_cell(food_address)
-   interaction.reply(food[choose].v)
+      const searchfood = interaction.options.getString('查詢')
+      for(var food_index = range.s.r;food_index <= range.e.r;food_index++){
+
+         if(food[xlsx.utils.encode_cell({c: 0, r:food_index})].v === searchfood){
+            const food_adder = food[xlsx.utils.encode_cell({c: 1, r:food_index})]
+            interaction.reply(`${searchfood} 是由 ${food_adder.v} 添加!! `)
+            return;
+         }
+      }
+      interaction.reply(`名單中找不到 ${searchfood} `)
    }
 
 }
